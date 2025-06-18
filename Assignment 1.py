9a6e1d8a3b81ade1dbbd119a2714f6a4b82e007c
spranshu999 <spranshu999@gmail.com>
2025-06-18 12:59:13 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,18 @@
+
+# Mathematical Operation
+a= int(input('Number_1: '))
+b=int(input('Number_2: '))
+Addition = a+b
+print("Addition: ", Addition)
+Subtraction = b-a
+print('Subtraction:',Subtraction)
+Multiplication = a*b
+print('Multiplication: ',Multiplication)
+Division = b/a
+print('Division: ',Division)
+
+# Personalized Greeting
+d = str(input('First Name: '))
+e= str(input('Last Name: '))
+Greetings = "Hello " + d +" "+ e + " welcome to Python program."
+print(Greetings)
